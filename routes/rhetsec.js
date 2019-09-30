a5e3dfb5a57d44b663ecec6f48d1a32585f2310f
Jenny <linuxfingers@gmail.com>
2019-09-29 20:05:08 -0400

    FORGOT if (req.user){

--- routes/rhetsec.js
+++ routes/rhetsec.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 //if logged in, else redirect
 //router.get('/', function(req, res, next) {
+//	if (req.user){
 //		res.render('rhetsec', {user: req.user, title: 'visitor@linuxfingers:~/project_desc$'});
 //		}
 //		else{
@@ -15,6 +16,7 @@ var router = express.Router();
 //****Main Section*****//
 
 router.get('/', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/index', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec$'});
 	}
 	else{
@@ -23,6 +25,7 @@ router.get('/', function(req,res,next) {
 });
 
 router.get('/about', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/about', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec$'});
 	}
 	else{
@@ -31,6 +34,7 @@ router.get('/about', function(req,res,next) {
 });
 
 router.get('/limitations', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/limitations', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/limitations$'});
 	}
 	else{
@@ -39,6 +43,7 @@ router.get('/limitations', function(req,res,next) {
 });
 
 router.get('/rationale', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/rationale', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/rationale$'});
 	}
 	else{
@@ -47,6 +52,7 @@ router.get('/rationale', function(req,res,next) {
 });
 
 router.get('/assessment', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/assessment', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/self_assessment$'});
 	}
 	else{
@@ -57,6 +63,7 @@ router.get('/assessment', function(req,res,next) {
 //******Phase 0******//
 
 router.get('/phase0/', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phaseprofit/index', {user: req.user, title:'visitor@linuxfingers.com:/rhetsec/phase0$'});
 	}
 	else{
@@ -67,6 +74,7 @@ router.get('/phase0/', function(req,res,next) {
 //******Phase 1******//
 
 router.get('/phase1/', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase1/index', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase1$'});
 	}
 	else{
@@ -75,6 +83,7 @@ router.get('/phase1/', function(req,res,next) {
 });
 
 router.get('/phase1/bibliography', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase1/bibliography', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase1/bibliography$'});
 	}
 	else{
@@ -83,6 +92,7 @@ router.get('/phase1/bibliography', function(req,res,next) {
 });
 
 router.get('/phase1/definitions', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase1/definitions', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase1/technical_definitions$'});
 	}
 	else{
@@ -93,6 +103,7 @@ router.get('/phase1/definitions', function(req,res,next) {
 //Email Analyses//
 
 router.get('/phase1/email_analysis/', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase1/email_analysis/index', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase1/email_analysis$'});
 	}
 	else{
@@ -101,6 +112,7 @@ router.get('/phase1/email_analysis/', function(req,res,next) {
 });
 
 router.get('/phase1/email_analysis/bulk', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase1/email_analysis/bulk', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase1/email_analysis/bulk_phishing_analysis$'});
 	}
 	else{
@@ -109,6 +121,7 @@ router.get('/phase1/email_analysis/bulk', function(req,res,next) {
 });
 
 router.get('/phase1/email_analysis/clonephishing', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase1/email_analysis/clonephishing', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase1/email_analysis/clonephishing_analysis$'});
 	}
 	else{
@@ -117,6 +130,7 @@ router.get('/phase1/email_analysis/clonephishing', function(req,res,next) {
 });
 
 router.get('/phase1/email_analysis/malspam', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase1/email_analysis/malspam', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase1/email_analysis/malspam_analysis$'});
 	}
 	else{
@@ -125,6 +139,7 @@ router.get('/phase1/email_analysis/malspam', function(req,res,next) {
 });
 
 router.get('/phase1/email_analysis/pse', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase1/email_analysis/pse', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase1/email_analysis/pse_analysis$'});
 	}
 	else{
@@ -133,6 +148,7 @@ router.get('/phase1/email_analysis/pse', function(req,res,next) {
 });
 
 router.get('/phase1/email_analysis/spearphishing', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase1/email_analysis/spearphishing', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase1/email_analysis/spearphishing_analysis$'});
 	}
 	else{
@@ -141,6 +157,7 @@ router.get('/phase1/email_analysis/spearphishing', function(req,res,next) {
 });
 
 router.get('/phase1/email_analysis/corpus', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase1/email_analysis/corpus', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase1/email_analysis/email_corpus$'});
 	}
 	else{
@@ -151,6 +168,7 @@ router.get('/phase1/email_analysis/corpus', function(req,res,next) {
 //******Phase 2******//
 
 router.get('/phase2/', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase2/index', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase2$'});
 	}
 	else{
@@ -159,6 +177,7 @@ router.get('/phase2/', function(req,res,next) {
 });
 
 router.get('/phase2/training/', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase2/training/index', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase2/training$'});
 	}
 	else{
@@ -167,6 +186,7 @@ router.get('/phase2/training/', function(req,res,next) {
 });
 
 router.get('/phase2/training/phished', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase2/training/phished', {user: req.user, title: 'visitor@linuxfingers:~/rhetsec/phase2/training/phished$'});
 	}
 	else{
@@ -175,6 +195,7 @@ router.get('/phase2/training/phished', function(req,res,next) {
 });
 
 router.get('/phase2/training/elogin', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phase2/training/elogin', {user: req.user, layout: 'false'});
 	}
 	else{
@@ -185,6 +206,7 @@ router.get('/phase2/training/elogin', function(req,res,next) {
 //******Phase Profit******//
 
 router.get('/phaseprofit/', function(req,res,next) {
+	if (req.user){
 		res.render('rhetsec/phaseprofit/index', {user: req.user, title:'visitor@linuxfingers.com:/rhetsec/phase_profit$'});
 	}
 	else{
