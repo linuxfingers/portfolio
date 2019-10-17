6c97d27623aea483866224224197e93d7bc05d89
Jenny <linuxfingers@gmail.com>
2019-10-16 23:01:49 -0400

    writing section edits

--- routes/writing.js
+++ routes/writing.js
@@ -5,24 +5,20 @@ router.get('/', function(req, res, next) {
 		res.render('writing', {user: req.user, title: 'visitor@linuxfingers:~/previous_works$'});
 });
 
-router.get('/comm', function(req, res, next) {
-		res.render('writing/comm', {user: req.user, title: 'visitor@linuxfingers:~/4chan_paper$'});
-});
-
-router.get('/mpca', function(req, res, next) {
-		res.render('writing/mpca', {user: req.user, title: 'visitor@linuxfingers:~/mpca_2016$'});
-});
-
-router.get('/seo', function(req, res, next) {
-		res.render('writing/seo', {user: req.user, title: 'visitor@linuxfingers:~/seo$'});
-});
-
-router.get('/poetry', function(req, res, next) {
-		res.render('writing/poetry', {user: req.user, title: 'visitor@linuxfingers:~/seo$'});
+router.get('/digispace', function(req, res, next) {
+		res.render('writing/mpca', {user: req.user, title: 'visitor@linuxfingers:~/digital_spaces$'});
 });
 
 router.get('/gonzo', function(req, res, next) {
-		res.render('writing/gonzo', {user: req.user, title: 'visitor@linuxfingers:~/seo$'});
+	res.render('writing/gonzo', {user: req.user, title: 'visitor@linuxfingers:~/gonzo&&narrative$'});
+});
+
+router.get('/other', function(req, res, next) {
+		res.render('writing/other', {user: req.user, title: 'visitor@linuxfingers:~/other_writing$'});
+});
+
+router.get('/poetry', function(req, res, next) {
+		res.render('writing/poetry', {user: req.user, title: 'visitor@linuxfingers:~/poetry$'});
 });
 
 module.exports = router;
