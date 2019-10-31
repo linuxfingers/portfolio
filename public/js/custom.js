6b392fb53571a8e41cf1e67f331629d9e55c3fe2
jenny lynn mead <linuxfingers@gmail.com>
2019-10-31 15:22:49 -0400

    visuals are ready for live

--- public/js/custom.js
+++ public/js/custom.js
@@ -45,123 +45,12 @@ jQuery(document).ready(function() {
   });
 });
 
-//folder animation
-
-var counter = 0;
-function resetImage(){
-     document.getElementById("initFolder").src = "/icons/folder.svg";
-     if(counter === 2)
-       document.getElementById('initFolder').style.visibility = "show";
-}
-
-function folderTimer() {
-counter++;
-  if (counter === 2)
-  {
-	document.getElementById("offFolder").src = "/icons/openfolder.svg";
-  }else{
-     document.getElementById("offFolder").src = "/icons/folder.svg";
-  }
-  setTimeout(function(){
-     resetImage();
-  }, 3000)
-
-}
-
-var counter = 0;
-function resetImage(){
-     document.getElementById("defFolder").src = "/icons/folder.svg";
-     if(counter === 2)
-       document.getElementById('defFolder').style.visibility = "show";
-}
-
-function folderTimer() {
-counter++;
-  if (counter === 2)
-  {
-	document.getElementById("wriFolder").src = "/icons/openfolder.svg";
-  }else{
-     document.getElementById("wriFolder").src = "/icons/folder.svg";
-  }
-  setTimeout(function(){
-     resetImage();
-  }, 3000)
-
-}
-
-var counter = 0;
-function resetImage(){
-     document.getElementById("vaFolder").src = "/icons/folder.svg";
-     if(counter === 2)
-       document.getElementById('vaFolder').style.visibility = "show";
-}
-
-function folderTimer() {
-counter++;
-  if (counter === 2)
-  {
-	document.getElementById("aboutFolder").src = "/icons/openfolder.svg";
-  }else{
-     document.getElementById("aboutFolder").src = "/icons/folder.svg";
-  }
-  setTimeout(function(){
-     resetImage();
-  }, 3000)
-
-}
 
 //draggable modals
 
-$('#initFolder').click(function() {
-	var modalDiv = $('#initModal');
-  modalDiv.modal({backdrop: false, show: true});
-
-  $('.modal-dialog').draggable({
-    handle: ".modal-header"
-  });
+$('.modal-content').resizable({
+  //alsoResize: ".modal-dialog",
+  minHeight: 300,
+  minWidth: 300
 });
-
-$('#offFolder').click(function() {
-	var modalDiv = $('#offModal');
-  modalDiv.modal({backdrop: false, show: true});
-
-  $('.modal-dialog').draggable({
-    handle: ".modal-header"
-  });
-});
-
-$('#defFolder').click(function() {
-	var modalDiv = $('#defModal');
-  modalDiv.modal({backdrop: false, show: true});
-
-  $('.modal-dialog').draggable({
-    handle: ".modal-header"
-  });
-});
-
-$('#wriFolder').click(function() {
-	var modalDiv = $('#wriModal');
-  modalDiv.modal({backdrop: false, show: true});
-
-  $('.modal-dialog').draggable({
-    handle: ".modal-header"
-  });
-});
-
-$('#vaFolder').click(function() {
-	var modalDiv = $('#vaModal');
-  modalDiv.modal({backdrop: false, show: true});
-
-  $('.modal-dialog').draggable({
-    handle: ".modal-header"
-  });
-});
-
-$('#aboutFolder').click(function() {
-	var modalDiv = $('#aboutModal');
-  modalDiv.modal({backdrop: false, show: true});
-
-  $('.modal-dialog').draggable({
-    handle: ".modal-header"
-  });
-});
+$('.modal-dialog').draggable();
