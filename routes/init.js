a7a7661bc84e6de876deba64769e703265ff77ac
Jenny <linuxfingers@gmail.com>
2019-12-03 16:30:21 -0500

    going live w/o pwd

--- routes/init.js
+++ routes/init.js
@@ -2,30 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req,res,next) {
-	if (req.user){
 		res.render('init/theproject', {user: req.user, title: 'visitor@linuxfingers:~/portfolio/the_project$'});
-	}
-	else{
-		res.redirect('/signin');
-	}
 });
 
 router.get('/rhet101', function(req,res,next) {
-	if (req.user){
 		res.render('init/rhet101', {user: req.user, title: 'visitor@linuxfingers:~/portfolio/rhetoric_101$'});
-	}
-	else{
-		res.redirect('/signin');
-	}
 });
 
 router.get('/bibliography', function(req,res,next) {
-	if (req.user){
 		res.render('init/bibliography', {user: req.user, title: 'visitor@linuxfingers:~/portfolio/bibliography$'});
-	}
-	else{
-		res.redirect('/signin');
-	}
 });
 
 module.exports = router;
--- routes/offensive.js
+++ routes/offensive.js
@@ -4,12 +4,7 @@ var router = express.Router();
 //main offensive//
 
 router.get('/', function(req,res,next) {
-	if (req.user){
 		res.render('offensive/index', {user: req.user, title: 'visitor@linuxfingers:~/offensive/readme$'});
-	}
-	else{
-		res.redirect('/signin');
-	}
 });
 
 
@@ -17,66 +12,31 @@ router.get('/', function(req,res,next) {
 //Email Analyses//
 
 router.get('/phishing/analysis', function(req,res,next) {
-	if (req.user){
 		res.render('offensive/phishing/analysis/index', {user: req.user, title: 'visitor@linuxfingers:~/phishing/analysis_info$'});
-	}
-	else{
-		res.redirect('/signin');
-	}
 });
 
 router.get('/phishing/analysis/bulk', function(req,res,next) {
-	if (req.user){
 		res.render('offensive/phishing/analysis/bulk', {user: req.user, title: 'visitor@linuxfingers:~/phishing/bulk_phishing_analysis$'});
-	}
-	else{
-		res.redirect('/signin');
-	}
 });
 
 router.get('/phishing/analysis/clonephishing', function(req,res,next) {
-	if (req.user){
 		res.render('offensive/phishing/analysis/clonephishing', {user: req.user, title: 'visitor@linuxfingers:~/phishing/clonephishing_analysis$'});
-	}
-	else{
-		res.redirect('/signin');
-	}
 });
 
 router.get('/phishing/analysis/malspam', function(req,res,next) {
-	if (req.user){
 		res.render('offensive/phishing/analysis/malspam', {user: req.user, title: 'visitor@linuxfingers:~/phishing/malspam_analysis$'});
-	}
-	else{
-		res.redirect('/signin');
-	}
 });
 
 router.get('/phishing/analysis/pse', function(req,res,next) {
-	if (req.user){
 		res.render('offensive/phishing/analysis/pse', {user: req.user, title: 'visitor@linuxfingers:~/phishing/pse_analysis$'});
-	}
-	else{
-		res.redirect('/signin');
-	}
 });
 
 router.get('/phishing/analysis/spearphishing', function(req,res,next) {
-	if (req.user){
 		res.render('offensive/phishing/analysis/spearphishing', {user: req.user, title: 'visitor@linuxfingers:~/phishing/spearphishing_analysis$'});
-	}
-	else{
-		res.redirect('/signin');
-	}
 });
 
 router.get('/phishing/analysis/corpus', function(req,res,next) {
-	if (req.user){
 		res.render('offensive/phishing/analysis/corpus', {user: req.user, title: 'visitor@linuxfingers:~/phishing/email_corpus$'});
-	}
-	else{
-		res.redirect('/signin');
-	}
 });
 
 
