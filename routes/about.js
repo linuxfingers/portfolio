0283e9aa4053947c9473df9b142fe582a9b3a1a1
linuxfingers <linuxfingers@pm.me>
2020-08-11 21:55:53 -0400

    clean up

--- routes/about.js
+++ routes/about.js
@@ -5,8 +5,8 @@ router.get('/cv', function(req, res, next) {
 	res.render('about/cv', {user: req.user, title: 'visitor@linuxfingers:~/about/cv$'});
 });
 
-router.get('/jen', function(req, res, next) {
-	res.render('about/jen', {user: req.user, title: 'visitor@linuxfingers:~/about/jen$'});
+router.get('/eva', function(req, res, next) {
+	res.render('about/eva', {user: req.user, title: 'visitor@linuxfingers:~/about/eva$'});
 });
 
 router.get('/provsec', function(req, res, next) {
